da204f9f76129d53ffcf22e1921c719c9cf11889
eohneey <imdnjs423@gmail.com>
2025-08-25 01:37:19 +0900

    fix: AI 코스 추천 결과 지도 줌레벨 수정

--- src/pages/ai/AiResult.jsx
+++ src/pages/ai/AiResult.jsx
@@ -159,7 +159,7 @@ const AiResult = () => {
                       markers={responseData?.data?.courses || []}
                       className="h-[25rem] aspect-[317.01/250.00]"
                       sectionClassName="pb-[3rem] pt-[-1rem]"
-                      zoomLevel={3}
+                      zoomLevel={2}
                       fitBounds={false}
                     />
 
